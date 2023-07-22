f284cf05f598049431c1ffa1aa3374aaa2a7196d
Ben Miles <bcgm3studios@gmail.com>
2023-07-22 13:07:02 -0400

    Initial commit

---
+++ awmdata/_vti_cnf/awmlib4.js
@@ -0,0 +1,9 @@
+vti_encoding:SR|utf8-nl
+vti_author:SR|bbretail
+vti_modifiedby:SR|bbretail
+vti_timelastmodified:TR|08 May 2008 10:11:53 -0000
+vti_timecreated:TR|08 May 2008 10:11:53 -0000
+vti_cacheddtm:TX|08 May 2008 10:11:53 -0000
+vti_filesize:IR|46644
+vti_extenderversion:SR|5.0.2.2634
+vti_backlinkinfo:VX|
---
+++ cgi/_vti_cnf/formmail.pl
@@ -0,0 +1,6 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|28 Apr 2008 18:29:10 -0000
+vti_extenderversion:SR|5.0.2.2634
+vti_cacheddtm:TX|28 Apr 2008 18:29:10 -0000
+vti_filesize:IR|75253
+vti_backlinkinfo:VX|mailinglist.htm
